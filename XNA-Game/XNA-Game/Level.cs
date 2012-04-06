6c0b0ec4b70918b52f724f2d18c18b2f8e6ef3e8
Lizzy Burl <lizzyburl@gmail.com>
2012-04-06 02:26:20 -0400

    Modified Level Class

--- XNA-Game/XNA-Game/Level.cs
+++ XNA-Game/XNA-Game/Level.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using XNA_Game.Sprites;
+using XNA_Game.Sprites.Blocks;
 using Microsoft.Xna.Framework;
 
 namespace XNA_Game.Levels
@@ -14,6 +15,22 @@ namespace XNA_Game.Levels
 
         public Level(string file)
         {
+            int row=-1;
+            int column=0;
+            foreach (string s in file.Split())
+            {
+                foreach (char c in s)
+                {
+                    if(c == '0')
+                    level[row][column] = new ;
+                    else if (c == '1')
+                        level[row][column] =new StaticBlock();
+                    else if (c == 'X')
+
+                    column++;
+                }
+                    row++;
+            }
             int i = 0;
         }
     }
